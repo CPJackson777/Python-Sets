1772fa9585b09535580a9fc605f25bf0b5b9c979
Charles Jackson <cpjackson572@gmail.com>
2020-01-14 22:58:49 -0600

    thought I completed the exercise before but realized after previous commit that I didnt. Now it's really finished

--- python-sets.py
+++ python-sets.py
@@ -24,3 +24,18 @@ print(showroom)
 # You've sold one of your cars. Remove it from the set with the discard() method.
 showroom.discard('Camry')
 print(showroom)
+
+# Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
+junkyard = {'taurus', 'accord', 'Ghibli', '720s', '4runner'}
+
+# Use the intersection method to see which cars exist in both the showroom and that junkyard.
+existInBoth = showroom.intersection(junkyard)
+print(existInBoth)
+
+# Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
+showroom = showroom.union(junkyard)
+print(showroom) 
+
+# Use the discard() method to remove any cars that you acquired from the junkyard that you do not want in your showroom.
+showroom.discard("4runner")
+print(showroom)
